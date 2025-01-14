55357f1dd80838c16ab5c93ea04b24506d48f066
unknown <223103@lab-ocs.local>
2025-01-14 11:02:04 +0900

    システム系

--- phpfile/question_answer.php
+++ phpfile/question_answer.php
@@ -2,9 +2,8 @@
 
     session_start();
 
-    $SNumber = $_SESSION['student_number'];
-    $PLName = $_POST['license_name'];
-    $PGDay = $_POST['getday'];
+    $PQNumber = $_POST['question_number'];
+    $PAnswer = $_POST['answer'];
 
     try{
         $dsn='mysql:host=database-2.c3cwkcssqi74.ap-northeast-3.rds.amazonaws.com;dbname=SCHOOL_WORKER;charset=utf8';
@@ -13,18 +12,16 @@
         $dbh = new PDO($dsn, $username, $password);
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $sql = 'INSERT INTO LICENSE (student_number, license_name, getday)
-                VALUES (:SNumber, :PLName, :PGDay);';
+        $sql = 'UPDATE QUESTION SET answer = :PAnswer, adatetime = now() WHERE question_number = :PQNumber;';
 
         $stmt=$dbh->prepare($sql);
-        $stmt->bindParam(':SNumber', $SNumber, PDO::PARAM_STR);
-        $stmt->bindParam(':PLName', $PLName, PDO::PARAM_STR);
-        $stmt->bindParam(':PGDay', $PGDay, PDO::PARAM_STR);
+        $stmt->bindParam(':PQNumber', $PQNumber, PDO::PARAM_STR);
+        $stmt->bindParam(':PAnswer', $PAnswer, PDO::PARAM_STR);
         $stmt->execute();
 
         $dbh = null;
 
-        header('Location:license.php');
+        header('Location:question.php');
     }catch (PDOException $e){
         print('Error:'.$e->getMessage());
         die();

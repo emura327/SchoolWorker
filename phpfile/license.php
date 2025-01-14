f6a7ac966fa8bc4f0995229e991fa4e05e5d1e50
unknown <223119@lab-ocs.local>
2025-01-14 13:57:14 +0900

    取得日カレンダー追加

--- phpfile/license.php
+++ phpfile/license.php
@@ -8,29 +8,19 @@
 </head>
 <body>
     <?php include('header_site.php');?>
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-        crossorigin="anonymous"
-    />
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-      crossorigin="anonymous"
-    ></script>
+
 <form action="/form.php" method="post">
-    <ul>
+    <ul class="uull">
         <li class="name">
             <div>
                 <label for="name">資格名</label>
-                <input type="text" id="name" name="name" size="60">
+                <input type="text">
             </div>
         </li>
         <li class="acquisition">
             <div>
-                <label for="message">取得日</label>
-                <label class="date-edit"><input type="date" value="2025-01-01" /></label>
+                <label class="message">取得日</label>
+                <label class="date-edit"><input class="date" value="2025-01-01" /></label>
             </div>
         </li>
     </ul>
